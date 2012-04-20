f4a0896d5e0ff45148c8f613d471af938aa68f19
jamescaca <james0104040254@gmail.com>
2012-04-21 01:40:10 +0800

    modify a little

--- TableAllocationProj/src/com/tap/bizlogic/OrderLogic.java
+++ TableAllocationProj/src/com/tap/bizlogic/OrderLogic.java
@@ -11,8 +11,6 @@ import com.tap.tableordersys.Table;
 import com.tap.usersys.Operator;
 
 public class OrderLogic {
-
-
 	/*
 	 * 向固定饭店的某个restaurant 添加order
 	 * param需要提前定义好
@@ -30,7 +28,7 @@ public class OrderLogic {
 		// 这里开始调用添加order的logic
 		// table需要提前设置好
 		//添加到waitinglist部分待完善waitinglist
-		//order需要一个可以将id初始化的constructor
+		//order需要一个可以将id初始化的constructor或者id直接被自动初始化
 		Table tableCheck = checkNewTableExist(restaurant);
 		if (tableCheck != null) {
 			// assign table
@@ -44,7 +42,11 @@ public class OrderLogic {
 	}
 
 	/*
-	 * 不用拼桌的时候 查看是否有剩余的没人的桌子 返回table id
+	 * 不用拼桌的时候 查看是否有剩余的没人的桌子 返回table
+	 */
+	/**
+	 * @param restaurant
+	 * @return
 	 */
 	public Table checkNewTableExist(Restaurant restaurant) {
 		List<Table> tablelist = restaurant.getTableList();
@@ -59,14 +61,22 @@ public class OrderLogic {
 	/*
 	 * 到现场之后查询order
 	 */
+	/**
+	 * @return
+	 */
 	public Order searchOrder(){
 		//查询到之后return order
-		
+		//TODO
 		return null;
 	}
 
 	/*
 	 * 修改Order在数据库中的状态
+	 */
+	/**
+	 * @param o
+	 * @param orderState
+	 * @return
 	 */
 	public boolean modifyAnOrderState(Order o, int orderState) {
 		//查询到之后直接存储成功return true
@@ -76,6 +86,9 @@ public class OrderLogic {
 
 	/*
 	 * 从数据库里删除一个Order order cancel_int db.delete()
+	 */
+	/**
+	 * @return
 	 */
 	public boolean cancelAnOrders() {
 		//查询数据库找到即可删除
